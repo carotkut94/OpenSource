6c40b69470a75a4021b2084aa3dc381e30d27293
carotkut94 <carotkut12@gmail.com>
2017-02-21 11:49:02 +0530

    Added search functionality

--- app/src/main/java/com/death/yttorrents/MainActivity.java
+++ app/src/main/java/com/death/yttorrents/MainActivity.java
@@ -1,16 +1,27 @@
 package com.death.yttorrents;
 
 import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -30,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Image> images;
     private ProgressDialog pDialog;
     private GalleryAdapter mAdapter;
+    static  int count = 0;
+    String query;
     private RecyclerView recyclerView;
 
     @Override
@@ -70,21 +83,76 @@ public class MainActivity extends AppCompatActivity {
 
             }
         }));
-
-        fetchImages();
+        fetchImages(endpoint);
     }
 
-    private void fetchImages() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topbar,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.seedr)
+        {
+            count+=1;
+            Toast.makeText(MainActivity.this, "Sorting by minimum rating 8 on page "+count, Toast.LENGTH_SHORT).show();
+            String url = "https://yts.ag/api/v2/list_movies.json?minimum_rating=8&limit=50&page="+count;
+            fetchImages(url);
+        }
+        if(item.getItemId()==R.id.search)
+        {
+
+            LayoutInflater li = LayoutInflater.from(MainActivity.this);
+            View dialogView = li.inflate(R.layout.custom_query, null);
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    MainActivity.this);
+            // set title
+            alertDialogBuilder.setTitle("Search Movie");
+            // set custom dialog icon
+            alertDialogBuilder.setIcon(R.drawable.ic_icon);
+            // set custom_dialog.xml to alertdialog builder
+            alertDialogBuilder.setView(dialogView);
+            final EditText userInput = (EditText) dialogView
+                    .findViewById(R.id.et_input);
+            // set dialog message
+            alertDialogBuilder
+                    .setCancelable(false)
+                    .setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    query = userInput.getText().toString();
+                                    String dataUrl = "https://yts.ag/api/v2/list_movies.json?query_term="+query+"&limit=30";
+                                    fetchImages(dataUrl);
+                                }
+                            })
+                    .setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+                                    dialog.cancel();
+                                }
+                            });
+            // create alert dialog
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            // show it
+            alertDialog.show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void fetchImages(String url) {
         pDialog.setMessage("Downloading json...");
         pDialog.show();
 
-        JsonObjectRequest request = new JsonObjectRequest(endpoint, new JSONObject(),
+        JsonObjectRequest request = new JsonObjectRequest(url, new JSONObject(),
                 new Response.Listener<JSONObject>() {
+
                     @Override
                     public void onResponse(JSONObject response) {
-
-                        pDialog.hide();
+                        images.clear();
                         try {
                             JSONObject object = response.getJSONObject("data");
                             JSONArray array = object.getJSONArray("movies");
@@ -109,11 +177,14 @@ public class MainActivity extends AppCompatActivity {
                                 Log.e("URL:",jsonObject.getString("url"));
                                 image.setURL(jsonObject.getString("url"));
                                 images.add(image);
+
                             }
+                            pDialog.hide();
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                         mAdapter.notifyDataSetChanged();
+                        pDialog.hide();
                     }
 
                 }, new Response.ErrorListener() {
@@ -123,46 +194,8 @@ public class MainActivity extends AppCompatActivity {
                 pDialog.hide();
             }
         });
-
-//        JsonArrayRequest req = new JsonArrayRequest(endpoint,
-//                new Response.Listener<JSONArray>() {
-//                    @Override
-//                    public void onResponse(JSONArray response) {
-//                        Log.d(TAG, response.toString());
-//                        pDialog.hide();
-//
-//                        images.clear();
-//                        for (int i = 0; i < response.length(); i++) {
-//                            try {
-//                                Log.e("LENGTH",""+response.length());
-//                                JSONObject object = response.getJSONObject(i);
-//                                Image image = new Image();
-//                                image.setName(object.getString("name"));
-//                                JSONObject url = object.getJSONObject("url");
-//                                image.setSmall(url.getString("small"));
-//                                image.setMedium(url.getString("medium"));
-//                                image.setLarge(url.getString("large"));
-//                                image.setTimestamp(object.getString("timestamp"));
-//
-//                                images.add(image);
-//
-//                            } catch (JSONException e) {
-//                                Log.e(TAG, "Json parsing error: " + e.getMessage());
-//                            }
-//                        }
-//
-//                        mAdapter.notifyDataSetChanged();
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.e(TAG, "Error: " + error.getMessage());
-//                pDialog.hide();
-//            }
-//        });
-
-        // Adding request to request queue
         AppController.getInstance().addToRequestQueue(request);
-
     }
+
+
 }
